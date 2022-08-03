d908e08277318db3d77cae0e8a510a48f771320f
1c0332zz <1c0332zz@gmail.com>
2022-08-03 17:37:34 +0900

    220803

---
+++ Algorithm/2차원리스트/01_test.py
@@ -0,0 +1,9 @@
+# matrix = []
+# for _ in range(3):
+#     line = list(map(int, input().split()))
+#     matrix.append(line)
+
+# print(matrix)
+
+matrix = [list(map(int, input().split())) for _ in range(3)]
+print(matrix)
---
+++ Algorithm/2차원리스트/02_test.py
@@ -0,0 +1,5 @@
+# 행렬의 크기가 입력으로 주어지는 경우
+n = 8
+m = 7
+
+matrix = [list(map(int, input().split())) for _ in range(8)]
