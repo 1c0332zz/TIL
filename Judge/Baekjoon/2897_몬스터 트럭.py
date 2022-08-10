f6b642daf5c878652979a70b63bb8c9a7113a906
1c0332zz <1c0332zz@gmail.com>
2022-08-10 19:21:43 +0900

    220810

--- Judge/Baekjoon/2897_몬스터 트럭.py
+++ Judge/Baekjoon/2897_몬스터 트럭.py
@@ -3,18 +3,40 @@ from pprint import pprint
 import sys
 sys.stdin = open("2897_몬스터 트럭.txt")
 
+
+
+
+
+
+
+
+
+
 # 행, 열, 주차장 입력받음
-R, C = map(int, input().split())
-matrix = [list(input().split()) for _ in range(C)]
-# pprint(matrix) 
-# >>> [['#..#'], 
-# >>>  ['..X.'], 
-# >>>  ['..X.'], 
-# >>>  ['#XX#']]
+# R, C = map(int, input().split())
+# parking = [list(input().split()) for _ in range(R)]
 
-# 델타 탐색?
-for i in matrix:
-    for j in i:
-        if matrix[i][j] == '.':
-            if matrix[i][j+1] =='.':
+# all, one, two, three, four = 0, 0, 0, 0, 0
+# for i in range(R - 1):  # 인덱스 벗어나지 않게 설정
+#     for j in range(C -1):
 
+#         arr = []
+#         for x in range(2):  # 혜빈이의 차 크기
+#             for y in range(2):
+#                 arr.append(parking[i + x][j + y])
+
+#         if '#' in arr:
+#             continue
+        
+#         if arr.count('.') == 4:
+#             all += 1
+#         elif arr.count('X') == 4:
+#             four += 1
+#         elif arr.count('X') == 3:
+#             three += 1
+#         elif arr.count('X') == 2:
+#             two += 1
+#         elif arr.count('X') == 1:
+#             one += 1
+
+# print(all, one, two, three, four, sep='\n')
---
+++ Judge/Baekjoon/4673_셀프 넘버.py
@@ -0,0 +1,11 @@
+numbers = list(range(1, 10_001))
+remove_list=[]    #생성자 리스트
+for num in numbers:
+    for n in str(num):
+        num += int(n)
+    if num <= 10_000:
+        remove_list.append(num)
+for remove_num in set(remove_list):
+    numbers.remove(remove_num)
+for self_num in numbers:
+    print(self_num)
