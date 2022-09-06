d280fef895624717861b97ebc669535e1ae51e47
1c0332zz <1c0332zz@gmail.com>
2022-09-06 15:52:48 +0900

    220906

--- Judge/programmers/pro_완주하지 못한 선수.py
+++ Judge/programmers/pro_완주하지 못한 선수.py
@@ -2,22 +2,20 @@
 def solution(participant, completion):
     answer = ''
     dict_ = {}
-    # sum_으로 한개의 리스트로 만들어준 뒤
-    sum_ = participant + completion
     # 딕셔너리 안에 다 담는다.
-    for i in sum_:
+    for i in participant:
         if i not in dict_:
             dict_[i] = 1
         else:
             dict_[i] += 1
 
 
-    # # 값이 2면 완주함
-    # for k, v in dict_.items():
-    #     if v != 2:
-    #         answer = k      
+    for i in completion:
+        if dict_[i] == 1:
+            del dict_[i]
+        else:
+            dict_[i] -= 1
 
-    return answer
-
+    return list(dict_.keys())[0]
 
 print(solution(	["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]))
