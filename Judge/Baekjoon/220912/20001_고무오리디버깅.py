650051da82a2333174e992f6bbf461abc4a6d3db
1c0332zz <1c0332zz@gmail.com>
2022-09-13 18:30:42 +0900

    220913

--- Judge/Baekjoon/220912/20001_고무오리디버깅.py
+++ Judge/Baekjoon/220912/20001_고무오리디버깅.py
@@ -1,9 +1,32 @@
 # https://www.acmicpc.net/problem/20001
-# import sys
-# sys.stdin = open("20001_고무오리디버깅.txt")
+import sys
+sys.stdin = open("20001_고무오리디버깅.txt", encoding='UTF-8')
 
-arr = []
+ducks = []
+duck = []
+d = input()
 
 while True:
-    duck = input()
-    arr.append(duck)
+    duck.append(input())
+
+    if duck[0] == "문제":
+        ducks.append(duck.pop(0))
+
+    if duck[0] == "고무오리":
+        if ducks[0] == "문제":
+            ducks.pop(0)
+            duck.pop(0)
+        else:
+            ducks.insert(0, "문제")
+            ducks.insert(0, "문제")
+            duck.pop(0)
+
+    if duck[0] == "고무오리 디버깅 끝":
+        if ducks not in "문제":
+            print("힝구")
+            break
+        else:
+            break
+
+if duck[1] == "고무오리 디버깅 끝" and duck[0] == "고무오리 디버깅 끝":
+    print("고무오리야 사랑해")
