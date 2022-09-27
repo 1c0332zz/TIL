0143cc84ee05d8900e28656b052e423d31365c23
1c0332zz <1c0332zz@gmail.com>
2022-09-27 21:21:44 +0900

    220927

---
+++ Judge/programmers/220926/2047.py
@@ -0,0 +1,7 @@
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QKsLaAy0DFAUq
+
+A = (input())
+print(A.upper())
+# for i in A:
+#     if i != "_":
+#         i.upper()
---
+++ Judge/programmers/220926/2050.py
@@ -0,0 +1,4 @@
+# https://swexpertacademy.com/main/code/problem/problemSubmitHistory.do?contestProbId=AV5QLGxKAzQDFAUq
+T = input() # 3
+for i in T:
+    print(ord(i) - 64, end =' ')
