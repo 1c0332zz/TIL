9f89c8578907eee80704042ba109096aa3cb5659
1c0332zz <1c0332zz@gmail.com>
2022-10-05 23:39:01 +0900

    221005

---
+++ Django/8일차/articles/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render, redirect
+from .forms import ArticleForm
+from .models import Article
+
+# Create your views here.
+
+
+def index(request):
+    articles = Article.objects.order_by("-pk")
+    context = {
+        "articles": articles,
+    }
+    return render(request, "articles/index.html", context)
+
+
+# def new(request):
+#     return render(request, "articles/new.html")
+
+
+def create(request):
+    if request.method == "POST":
+        article_form = ArticleForm(request.POST)
+        if article_form.is_valid():
+            article_form.save()
+            return redirect("articles:index")
+    else:
+        article_form = ArticleForm()
+    context = {
+        "article_form": article_form,
+    }
+    return render(request, "articles/new.html", context=context)
+
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        "article": article,
+    }
+    return render(request, "articles/detail.html", context)
+
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    if request.method == "POST":
+        article_form = ArticleForm(request.POST, instance=article)
+        if article_form.is_valid():
+            article_form.save()
+            return redirect("articles:detail", article.pk)
+    else:
+        article_form = ArticleForm(instance=article)
+    context = {
+        "article_form": article_form,
+    }
+    return render(request, "articles/update.html", context)
