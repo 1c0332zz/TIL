8f58df24fea8450e7f2d2a25bc4ede13e5dd20b5
1c0332zz <1c0332zz@gmail.com>
2022-11-12 00:21:22 +0900

    hello-c

--- C/HelloC.c
+++ C/HelloC.c
@@ -3,4 +3,6 @@
 int main(void)
 {
     printf("hello, world\n");
+    printf("안녕하세요\n");
+    return 0;
 }
---
+++ C/hellocpp.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "welcome C++" << endl;
+    return 0;
+}
