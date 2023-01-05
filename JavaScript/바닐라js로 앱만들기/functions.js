fa8ddfcff488f0d237cfa3b7a4ecafb0ea42c526
1c0332zz <1c0332zz@gmail.com>
2023-01-05 15:35:13 +0900

    자바스크립트 수정

--- JavaScript/바닐라js로 앱만들기/functions.js
+++
@@ -1,7 +0,0 @@
- function sayhello(nameOfPerson) {
-  console.log("Hello my name is C");
- }
-
-sayhello("nico");
-sayhello("dal");
-sayhello("lynn");
--- JavaScript/바닐라js로 앱만들기/test/EventListener.js
+++
@@ -1,7 +0,0 @@
-const title = document.querySelector("div.hello:first-child h1");
-
-function handleTitleClick() {
-    console.log("title was clicked!");
-}
-
-title.addEventListener("click", handleTitleClick);
